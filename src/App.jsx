6077722319ccef053cf6fedabcd60efdbc09ea32
Amara Stephens <amarastephens@Amaras-MBP.attlocal.net>
2021-10-25 12:58:19 -0500

    fin

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+const App = () => {
+  return <BrowserRouter>
+  <Switch>
+
+<Route exact path= "/">
+</Route>
+
+
+
+  </Switch>
+  
+  </BrowserRouter>;
+};
+
+export default App;
